6d17c68d568f272ee7b94f9844d60ecb92eb5885
Akshat250606 <77012063+Akshat250606@users.noreply.github.com>
2021-04-22 20:58:51 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,49 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+
+var dustbinObj, paperObject, groundObject;	
+var world;
+
+function setup() {
+	createCanvas(1600, 700);
+	rectMode(CENTER);
+
+	engine = Engine.create();
+	world = engine.world;
+	
+	dustbinObject=new dustbin(1200,650);
+	paperObject=new paper(200,450,40);
+	groundObject=new ground(width/2,670,width,20);
+	launcherObject = new launcher(paperObject.body, {x:500,y:300});
+	
+	Engine.run(engine);
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background("grey");
+  dustbinObject.display()
+  paperObject.display()
+  groundObject.display()
+  launcherObject.display()
+ 
+ // call function to display objects
+}
+
+function mouseDragged(){
+    Matter.Body.setPosition(paperObject.body, {x: mouseX , y: mouseY});
+}
+
+function mouseReleased(){
+	launcherObject.fly()
+    // call function fly() given in launcher class file
+}
+
+
+
+
+
